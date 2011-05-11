10c4291231a83de9490dde1a2d9161a4a41efc33
Pablo Hoffman <pablo@pablohoffman.com>
2011-05-11 14:35:40 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='scextras',
+      version='0.1',
+      license='BSD',
+      description='Scrapy helper functions and processors',
+      author='Scrapinghub',
+      author_email='info@scrapinghub.com',
+      url='http://github.com/scrapinghub/scextras',
+      packages=['scextras', 'scextras.processors'],
+      platforms = ['Any'],
+      classifiers = [ 'Development Status :: 4 - Beta',
+                      'License :: OSI Approved :: BSD License',
+                      'Operating System :: OS Independent',
+                      'Programming Language :: Python']
+)
