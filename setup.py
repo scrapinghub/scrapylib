dc239e3282e9979f515b34199ebb7aea266fc834
Daniel Graña <dangra@gmail.com>
2012-08-23 12:48:02 -0300

    increase package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='scrapylib',
-      version='0.1',
+      version='0.2',
       license='BSD',
       description='Scrapy helper functions and processors',
       author='Scrapinghub',
