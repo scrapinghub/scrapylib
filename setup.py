b928d6165fd6372aecbff3bfa3392daabc986f87
Daniel Graña <dangra@gmail.com>
2013-11-25 15:03:09 -0200

    repo is scrapylib

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='scrapylib',
-      version='1.0.1',
+      version='1.0.2',
       license='BSD',
       description='Scrapy helper functions and processors',
       author='Scrapinghub',
