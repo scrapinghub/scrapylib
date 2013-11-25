7e36deee5cdbae2bb7b9a78b2cd0b7ae28866def
Daniel Graña <dangra@gmail.com>
2013-11-25 09:08:59 -0200

    bump versiont to 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='scrapylib',
-      version='0.2',
+      version='1.0',
       license='BSD',
       description='Scrapy helper functions and processors',
       author='Scrapinghub',
