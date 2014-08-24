6cc5417aa71eafe0fd4f52d95713724d233a2857
Daniel Graña <dangra@gmail.com>
2014-08-24 01:22:32 -0300

    Bump version: 1.3.0 → 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapylib',
-    version='1.3.0',
+    version='1.4.0',
     license='BSD',
     description='Scrapy helper functions and processors',
     author='Scrapinghub',
